906472fab39fc0085665f73bdf44ef28af04ac34
iaakopyan <szafgvsdg@gmail.com>
2022-11-28 16:35:00 +0300

    upgraded output

--- lab1strategies.py
+++ lab1strategies.py
@@ -6,10 +6,11 @@ matrix = np.array([[8, 11, 9, 3, 5],
                   [9, 11, 4, 12, 6], #4
                   [11, 6, 11, 11, 1],
                   [8, 4, 0, 7, 7]])
-P = (0.55, 0.0, 0.18, 0.0, 0.26)
-Q = (0.0, 0.0, 0.18, 0.01, 0.8)
-lower_price = max([min(x) for x in matrix])
-upper_price = min([max(x) for x in np.rot90(matrix)])
+P = [0.55, 0.0, 0.18, 0.0, 0.26]
+Q = [0.0, 0.0, 0.18, 0.01, 0.8]
+
+lower_price = max([min(x) for x in matrix])#минмакс
+upper_price = min([max(x) for x in np.rot90(matrix)])#максмин
 a= {lower_price, upper_price}
 if len(a)!=1:
     print("\033[32mСедловой точки нет\033[39m")
